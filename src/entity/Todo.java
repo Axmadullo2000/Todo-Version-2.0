6057fc5ffce3b90c1aa15a891c44cf5517184517
Axmadullo2000 <axmadullo2000@gmail.com>
2025-05-29 21:36:38 +0500

    Todo with new features by the way, I added Record class, also I rewrite on my own

---
+++ src/entity/Todo.java
@@ -0,0 +1,32 @@
+package entity;
+
+public class Todo {
+    private String title;
+    private String description;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    private String status;
+}
